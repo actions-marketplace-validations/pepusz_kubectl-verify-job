246374497806073315b2dfc98e05e26d81461d8f
Peter Varga <pepusz@gmail.com>
2021-02-26 18:09:06 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Docker and Github Action for Kubernetes CLI
---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+# Extract the base64 encoded config data and write this to the KUBECONFIG
+echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
+export KUBECONFIG=/tmp/config
+
+
+counter=0
+result="$(kubectl get job "$*" -o jsonpath={.status.succeeded})"
+while [  "$result" = "0" -a $counter -lt 4 ]
+do
+  sleep 5
+  result="$(kubectl get job challenge-migration -o jsonpath={.status.succeeded})"
+  ((counter++))
+  echo $counter
+  echo "$result"
+done
+
+if [ "$result" = "0" ]
+then
+exit 1
+fi
